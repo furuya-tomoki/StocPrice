ad8ec39a79635f98c2dbe0183d1d5ed2eee86f57
github furuya-tomoki <github>
2021-10-20 01:56:46 +0900

    [Add]comment

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+ # データ解析ライブラリ
+ # DataFrameなど使用可能
 import pandas as pd
+ # 株価情報の取得が可能
 import yfinance as yf
+ # 散布図を作成するライブラリ
 import altair as alt
+ # streamlitに反映するライブラリ
 import streamlit as st
 
+ # サイトのタイトル
 st.title("株価可視化アプリ")
 
+ # サイトのサイドバー
+ # マークダウンで記述
 st.sidebar.write("""
 # GAFA 株価
 これは株価可視化ツールです。
@@ -14,39 +22,57 @@ st.sidebar.write("""
 st.sidebar.write("""
 ## 表示日数選択
 """)
-
+ # slider = スライダーツールの作成
+ # （"タイトル",最小値, 最大値, デフォルト値）
+ # 変数daysに代入
 days = st.sidebar.slider("日数", 1, 50, 20)
 
-
+ # f = 文字列の一部に変数や式をいれておき、それを実行時に置き換えるというもの
+ # daysには、("日数", 1, 50, 20)が代入されている
 st.write(f"""
 ### 過去 **{days}日間**  のGAFA株価
 """)
 
-
-@st.cache
+ # 読み取り速度を向上してくれる
+@st.cache 
+ # 関数名get_data(引数1=days, 引数2=tickers)
 def get_data(days, tickers):
+   # 変数df = DataFrame
     df = pd.DataFrame()
+    # for文 「company」の中に「tickersのキー」1つずつ代入
     for company in tickers.keys():
+      # 変数tkrにはyfinanceが反映
         tkr = yf.Ticker(tickers[company])
+         # history = 過去の情報
+         # period = 日付を Period として扱う
+         # d = dを用いることで数値を文字列中に代入
         hist = tkr.history(period=f'{days}d')
+         # index = 要素がlistの中で何番目かを示すもの
+         # strftime() = 任意のフォーマットの文字列に変換
         hist.index = hist.index.strftime('%d %B %Y')
+         # 「Close」カラムを指定
         hist = hist[['Close']]
+         # カラムを「company」の値に変更
         hist.columns = [company]
+         # T = データを横に変換
         hist = hist.T
+         # index.name = データフレームindexとcolumnsのタイトル
         hist.index.name = 'Name'
+         # cancat = 複数のpandas.DataFrame, pandas.Seriesを連結する
+         # このばあい、「dfとhist」が連結している
         df = pd.concat([df, hist])
     return df
 
-
+ # エラーが起きた時に、以下の処理が行われる
 try:
     st.sidebar.write("""
   ## 株価の範囲指定
   """)
-
     ymin, ymax = st.sidebar.slider(
         "範囲を選択してください。",
         0.0, 3500.0, (0.0, 3500.0)
     )
+     # tickersの中のデータ
     tickers = {
         'apple': 'AAPL',
         'facebook': 'FB',
@@ -68,12 +94,14 @@ try:
     }
     df = get_data(days, tickers)
 
+     # multiselect = マルチセレクト機能の追加（複数選択）
     companies = st.multiselect(
         "会社名を選択してください。",
         list(df.index),
         ["apple", "amazon", "google", "facebook"]
     )
 
+     # if文 = 選択されなかった場合の処理
     if not companies:
         st.error("少なくとも一社は選んでください。")
     else:
